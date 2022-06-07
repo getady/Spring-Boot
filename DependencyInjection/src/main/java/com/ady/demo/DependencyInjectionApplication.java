fc2e14657b4efd55669fd09d6b1d3e3373fef928
ammararab <ammararab777@gmail.com>
2022-06-07 17:20:03 +0530

    Dependency Injection

---
+++ DependencyInjection/src/main/java/com/ady/demo/DependencyInjectionApplication.java
@@ -0,0 +1,19 @@
+package com.ady.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class DependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext con = SpringApplication.run(DependencyInjectionApplication.class, args);
+		Customer c = con.getBean(Customer.class);
+		//Product p = con.getBean(Product.class);
+		
+		c.display();
+		//p.show();
+	}
+
+}
---
+++ DependencyInjection/src/main/java/com/ady/demo/Product.java
@@ -0,0 +1,31 @@
+package com.ady.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Product 
+{
+
+	private int pid;
+	private String pname;
+	
+	
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	
+	public void show()
+	{
+		System.out.println("Out of stockage....");
+	}
+	
+}
