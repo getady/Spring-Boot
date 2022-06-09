5cb777c89ae64b077521802ea50f50f8f64c0d69
ammararab <ammararab777@gmail.com>
2022-06-09 12:04:00 +0530

    Demo REST API

---
+++ DemoRestAPI/src/main/java/com/ady/demo/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.ady.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController
+{
+	// localhost:8080/hello
+		
+	@GetMapping("/hello")
+	public String helloWorld()
+	{
+		return "Hello Welcome to Rest API";
+	}
+	
+	
+}
+	
